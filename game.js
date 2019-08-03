b56dfa903b9ff3477470ab00ab859b500f1d1656
Rubaiyat Islam Rafat <rafat.mit.806@gmail.com>
2019-08-03 16:23:42 +0600

    Create game.js

---
+++ game.js
@@ -0,0 +1,10 @@
+var config = {
+                //type: Phaser.CANVAS,
+                width: 800,
+                height: 600,
+                //title: "DengueMan",
+                backgroundColor: 0xF0F0F0,
+                scene: [Scene001, Scene002]
+            }
+
+var game = new Phaser.Game(config);
